eed3bd4b2302488739d0d224e899daa644ebb04e
Erik G. Ingebrigtsen <100137114+Erik-G-I@users.noreply.github.com>
2025-02-25 14:30:59 +0100

    37 create basic map (#43)
    * Started implementing Map from leaflet, but it is not showing yet
    
    * Still not working
    
    * fixed basic map integration
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Morag0 <morten.aagotnes@gmail.com>

---
+++ frontend/src/components/map.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted } from "vue";
+import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
+import L from "leaflet"; // Import Leaflet itself
+
+onMounted(() => {
+  if (typeof window === "undefined") return; // Prevents SSR errors
+
+  // var mapContainer = document.getElementById("map");
+  // if (!mapContainer) return;
+
+  // var map = L.map(mapContainer).setView([51.505, -0.09], 13);
+  var map = L.map('map').setView([60.39, 5.32], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>',
+  }).addTo(map);
+});
+
+</script>
+
+<template>
+  <div id="map"></div>
+</template> 
+<style>
+  #map {
+    height: 400px;
+    width: 500px
+  }
+</style>
+
+
+
+
+
