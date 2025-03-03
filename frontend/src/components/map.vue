ae90acdecee8a40ff576d79d51ca6bedb24de8c4
Morag0 <morten.aagotnes@gmail.com>
2025-03-03 15:48:17 +0100

    Added a function that opens links in a new window

--- frontend/src/components/map.vue
+++ frontend/src/components/map.vue
@@ -10,7 +10,8 @@ onMounted(() => {
   // if (!mapContainer) return;
 
   // var map = L.map(mapContainer).setView([51.505, -0.09], 13);
-  var map = L.map('map').setView([60.39, 5.32], 13);
+  var map = L.map('map').setView([60.39, 5.32], 11
+  );
 
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
@@ -18,13 +19,13 @@ onMounted(() => {
   }).addTo(map);
 
   var remaKjøkkelvik = L.marker([60.382042650854174, 5.229395571867122]).addTo(map);
-  remaKjøkkelvik.bindPopup("<b>Rema1000 Kjøkkelvik</b><br>Her kan du levere klær, og kaste glass, metall og matavfall!").openPopup();
+  remaKjøkkelvik.bindPopup("<b>Rema1000 Kjøkkelvik</b><br>Her kan du levere klær, og kaste glass, metall og matavfall!");
 
   var stasjonStognafjell = L.marker([60.405207615694536, 5.210220229866483]).addTo(map);
-  stasjonStognafjell.bindPopup("<b>Stognafjellsvegen</b><br>Her kan du levere klær, og kaste glass, metall og matavfall").openPopup();
+  stasjonStognafjell.bindPopup("<b>Stognafjellsvegen</b><br>Her kan du levere klær, og kaste glass, metall og matavfall");
 
   var birMøllendal = L.marker([60.38080146477514, 5.354526442798485]).addTo(map);
-  birMøllendal.bindPopup("<b>BIR Møllendalsveien</b><br>For full overisikt se: <a href='https://bir.no/bli-kvitt-boss/sortering-i-miljoeparken/' id='popuplink'>bir.no</a>").openPopup();
+  birMøllendal.bindPopup("<b>BIR Møllendalsveien</b><br>For full overisikt over hva som kan leveres her, se: <a href='https://bir.no/bli-kvitt-boss/sortering-i-miljoeparken/' id='popuplink'>bir.no</a>");
 
   map.on('popupopen', function (e){
     document.getElementById('popuplink').addEventListener('click', function (event){
