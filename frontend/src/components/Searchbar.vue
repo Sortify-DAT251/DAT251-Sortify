b64faa6ac86bcd232c3eaab10d1306963ba7e0ad
Erik <erik.g.ingebrigtsen@gmail.com>
2025-03-10 09:44:56 +0100

    basic vue component to make searchbar modular. levenshtein not implemented

---
+++ frontend/src/components/Searchbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+function levenshtein(){
+    var input = document.getElementById("searchInput").value;
+    console.log(value);
+}
+</script>
+
+<template>
+    <div>
+        <input type="text" id="searchInput" onkeyup="levenshtein()" placeholder="Search..." />
+    </div>
+</template>
+
