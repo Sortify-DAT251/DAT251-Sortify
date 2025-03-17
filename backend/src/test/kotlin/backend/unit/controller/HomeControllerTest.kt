dcba9ae90193b5808e10c2109b2c029e4d93af65
Mampenda <amalie.mampenda@gmail.com>
2025-03-17 16:14:57 +0100

    Modified HomeControllerTest.kt (test should pass)

--- backend/src/test/kotlin/backend/unit/controller/HomeControllerTest.kt
+++ backend/src/test/kotlin/backend/unit/controller/HomeControllerTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
@@ -11,6 +12,15 @@ import org.hamcrest.Matchers.containsString
 
 @SpringBootTest
 @AutoConfigureMockMvc
+// Update your test classes to use the H2 database configuration instead of using separate applications-test.properties file
+@TestPropertySource(properties = [
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=update",
+    "spring.jpa.show-sql=true"
+])
 class HomeControllerTest {
 
     @Autowired
