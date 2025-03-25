0a2db36cbe838f4c7899cc632b27fc9a2b483d3f
Mampenda <amalie.mampenda@gmail.com>
2025-03-25 14:45:41 +0100

    Refactored Location and Waste

--- backend/src/main/kotlin/backend/repository/LocationRepository.kt
+++ backend/src/main/kotlin/backend/repository/LocationRepository.kt
@@ -1,9 +1,9 @@
 package backend.repository
 
-import backend.model.Locations
+import backend.model.Location
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface LocationsRepository : JpaRepository<Locations, Long>
+interface LocationRepository : JpaRepository<Location, Long>
