b093522667fdd57ff4aa7cd7d3f50563d54cf5cf
Morten Ågotnes <morten.aagotnes@gmail.com>
2025-03-26 13:55:01 +0100

    Added map and header

---
+++ frontend/sortify/app/component/header.tsx
@@ -0,0 +1,36 @@
+import styles from "../component/header.module.css";
+
+export default function Header() {
+    return (
+        <div className={styles.headerContainer}>
+            <header className={styles.header}>
+                <nav className={styles.nav}>
+                    {/* Logo */}
+                    <a href="/en/" className={styles.logo}>
+                        <img
+                            src="/sortify-header.png"
+                            alt="sortify logo"
+                            width="200"
+                            className={styles.logoImage}
+                        />
+                    </a>
+                    {/* Navigation Links */}
+                    <div className={styles.links}>
+                        <a href="/en/" className={styles.button}>
+                            Home
+                        </a>
+                        <a href="/en/stats" className={styles.button}>
+                            Stats
+                        </a>
+                        <a
+                            href="https://docs.astro.build/en/develop-and-build/"
+                            className={styles.button}
+                        >
+                            Documentation
+                        </a>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    );
+}
--- frontend/sortify/app/layout.tsx
+++ frontend/sortify/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "../app/component/header"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <div id="map"></div>
+
+      <Header />
+
+
+
       </body>
     </html>
   );
