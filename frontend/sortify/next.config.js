7800b176fc178d9530255533536512ec7dc71cef
Mampenda <amalie.mampenda@gmail.com>
2025-04-17 22:05:24 +0200

    oOdified next.config.js file

--- frontend/sortify/next.config.js
+++ frontend/sortify/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.(png|jpg|jpeg|gif|svg)$/i,
+            type: 'asset/resource',
+        });
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
